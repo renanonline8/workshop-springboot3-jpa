1ad3d971ee3b22170d89ee36bbe5e34dbd26a30f
Renan Santos Gomes <github@renansgomes.com.br>
2022-12-02 08:54:39 -0300

    Category

---
+++ src/main/java/com/renansgomesfsdev/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.renansgomesfsdev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.renansgomesfsdev.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
